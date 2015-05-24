682c520bc2a457390f8d2b6fabe1466779cfa66d
Sier Violencia <sierisimo@gmail.com>
2015-05-23 22:31:27 -0500

    Fixed issue with diagonals

--- js/nntron.js
+++ js/nntron.js
@@ -19,186 +19,192 @@ var __width = 600,
   requestAnimationFrame = w.requestAnimationFrame || w.webkitRequestAnimationFrame || w.msRequestAnimationFrame || w.mozRequestAnimationFrame;
 
 (function(canvas) {
-  var bgReady = playerImgReady = npcImgReady = false,
-    bgImage = new Image(),
-    playerImg = new Image(),
-    npcImg = new Image(),
-    ctx = canvas.getContext("2d"),
-    player = {
-      speed: __speed,
-      movements: {
+    var bgReady = playerImgReady = npcImgReady = false,
+      bgImage = new Image(),
+      playerImg = new Image(),
+      npcImg = new Image(),
+      ctx = canvas.getContext("2d"),
+      player = {
+        speed: __speed,
+        movements: {
+          up: true,
+          down: true,
+          left: false,
+          right: false
+        },
+        direction: DIRECTIONS.RIGHT,
+        points: [],
+        x: 0,
+        y: 0
+      },
+      npc = {
+        speed: __speed,
+        movements: {
+          up: true,
+          down: true,
+          left: false,
+          right: false
+        },
+        points: [],
+        direction: DIRECTIONS.LEFT,
+        level: 0,
+        x: 0,
+        y: 0
+      },
+      score = [0, 0]; //Score in the form: player, npc
+
+    bgImage.onload = function() {
+      bgReady = true;
+    };
+    bgImage.src = "img/cyber.jpg";
+
+    playerImg.onload = function() {
+      playerImgReady = true;
+    };
+    playerImg.src = "img/player.png";
+
+    npcImg.onload = function() {
+      npcImgReady = true;
+    };
+    npcImg.src = "img/npc.png";
+
+    function render() {
+      var ctx = ctx || glob.ctx,
+        player = glob.player,
+        npc = glob.npc;
+
+      player.points.push({
+        x: player.x,
+        y: player.y
+      });
+
+      if (player.direction === DIRECTIONS.RIGHT) {
+        player.x += 1;
+      } else if (player.direction === DIRECTIONS.LEFT) {
+        player.x -= 1;
+      } else if (player.direction === DIRECTIONS.UP) {
+        player.y -= 1;
+      } else if (player.direction === DIRECTIONS.DOWN) {
+        player.y += 1;
+      }
+
+      if (npc.direction === DIRECTIONS.LEFT) {
+        npc.x -= 1;
+      } else if (npc.direction === DIRECTIONS.LEFT) {
+        npc.x -= 1;
+      } else if (npc.direction === DIRECTIONS.UP) {
+        npc.y -= 1;
+      } else if (npc.direction === DIRECTIONS.DOWN) {
+        npc.y += 1;
+      }
+
+      if (bgReady) {
+        ctx.drawImage(bgImage, 0, 0);
+      }
+      if (playerImgReady) {
+        ctx.drawImage(playerImg, glob.player.x, glob.player.y);
+      }
+      if (npcImgReady) {
+        ctx.drawImage(npcImg, glob.npc.x, glob.npc.y);
+      }
+    }
+    glob.render = render;
+
+    glob.keys = {};
+    addEventListener('keydown', function(e) {
+      // glob.keys[e.keyCode] = true;
+      glob.key = e.keyCode;
+    }, false);
+
+    addEventListener('keyup', function(e) {
+      // delete glob.keys[e.keyCode];
+      delete glob.key;
+    }, false);
+
+    function resetScenario() {
+      this.player.x = __width / 4;
+      this.player.y = __height / 2;
+      this.player.movements = [];
+      this.player.direction = DIRECTIONS.RIGHT;
+      this.player.movements = {
         up: true,
         down: true,
         left: false,
         right: false
-      },
-      direction: DIRECTIONS.RIGHT,
-      points: [],
-      x: 0,
-      y: 0
-    },
-    npc = {
-      speed: __speed,
-      movements: {
+      };
+
+      this.npc.x = (__width / 4) * 3;
+      this.npc.y = __height / 2;
+      this.npc.movements = [];
+      this.npc.direction = DIRECTIONS.LEFT;
+      this.npc.movements = {
         up: true,
         down: true,
         left: false,
         right: false
-      },
-      points: [],
-      direction: DIRECTIONS.LEFT,
-      level: 0,
-      x: 0,
-      y: 0
-    },
-    score = [0, 0]; //Score in the form: player, npc
-
-  bgImage.onload = function() {
-    bgReady = true;
-  };
-  bgImage.src = "img/cyber.jpg";
-
-  playerImg.onload = function() {
-    playerImgReady = true;
-  };
-  playerImg.src = "img/player.png";
-
-  npcImg.onload = function() {
-    npcImgReady = true;
-  };
-  npcImg.src = "img/npc.png";
-
-  function render() {
-    var ctx = ctx || glob.ctx,
-      player = glob.player,
-      npc = glob.npc;
-
-    player.points.push({
-      x: player.x,
-      y: player.y
-    });
-
-    if (player.direction === DIRECTIONS.RIGHT) {
-      player.x += 1;
-    } else if (player.direction === DIRECTIONS.LEFT) {
-      player.x -= 1;
-    } else if (player.direction === DIRECTIONS.UP) {
-      player.y -= 1;
-    } else if (player.direction === DIRECTIONS.DOWN) {
-      player.y += 1;
+      };
     }
+    glob.resetScenario = resetScenario;
 
-    if (npc.direction === DIRECTIONS.LEFT) {
-      npc.x -= 1;
-    } else if (npc.direction === DIRECTIONS.LEFT) {
-      npc.x -= 1;
-    } else if (npc.direction === DIRECTIONS.UP) {
-      npc.y -= 1;
-    } else if (npc.direction === DIRECTIONS.DOWN) {
-      npc.y += 1;
-    }
-
-
-    if (bgReady) {
-      ctx.drawImage(bgImage, 0, 0);
-    }
-    if (playerImgReady) {
-      ctx.drawImage(playerImg, glob.player.x, glob.player.y);
-    }
-    if (npcImgReady) {
-      ctx.drawImage(npcImg, glob.npc.x, glob.npc.y);
-    }
-  }
-  glob.render = render;
-
-  glob.keys = {};
-
-  addEventListener('keydown', function(e) {
-    glob.keys[e.keyCode] = true;
-  }, false);
-
-  addEventListener('keyup', function(e) {
-    delete glob.keys[e.keyCode];
-  }, false);
-
-  function resetScenario() {
-    this.player.x = __width / 4;
-    this.player.y = __height / 2;
-    this.player.movements = [];
-    this.player.direction = DIRECTIONS.RIGHT;
-    this.player.movements = {
-      up: true,
-      down: true,
-      left: false,
-      right: false
-    };
-
-    this.npc.x = (__width / 4) * 3;
-    this.npc.y = __height / 2;
-    this.npc.movements = [];
-    this.npc.direction = DIRECTIONS.LEFT;
-    this.npc.movements = {
-      up: true,
-      down: true,
-      left: false,
-      right: false
-    };
-  }
-  glob.resetScenario = resetScenario;
-
-  function updateGame(modifier) {
-    var player = glob.player,
-      npc = glob.npc,
-      needReset = false;
-    //key arrow left pressed
-    if (37 in glob.keys) {
-      if (player.movements.left) {
-        player.x -= player.speed * modifier;
-        player.direction = DIRECTIONS.LEFT;
-        player.movements = {
-          left: false,
-          right: false,
-          up: true,
-          down: true
+    function updateGame(modifier) {
+      var player = glob.player,
+        npc = glob.npc,
+        needReset = false;
+      if (!!glob.key) {
+        //key arrow left pressed
+        // if (37 in glob.keys) {
+        if (37 === glob.key){
+          if (player.movements.left) {
+            player.x -= player.speed * modifier;
+            player.direction = DIRECTIONS.LEFT;
+            player.movements = {
+              left: false,
+              right: false,
+              up: true,
+              down: true
+            }
+          }
+      }
+      //key arrow up pressed
+      // if (38 in glob.keys) {
+      if (38 === glob.key) {
+        if (player.movements.up) {
+          player.y -= player.speed * modifier;
+          player.direction = DIRECTIONS.UP;
+          player.movements = {
+            left: true,
+            right: true,
+            up: false,
+            down: false
+          }
         }
       }
-    }
-    //key arrow up pressed
-    if (38 in glob.keys) {
-      if (player.movements.up) {
-        player.y -= player.speed * modifier;
-        player.direction = DIRECTIONS.UP;
-        player.movements = {
-          left: true,
-          right: true,
-          up: false,
-          down: false
+      //key arrow right pressed
+      // if (39 in glob.keys) {
+      if (39 === glob.key) {
+        if (player.movements.right) {
+          player.x += player.speed * modifier;
+          player.direction = DIRECTIONS.RIGHT;
+          player.movements = {
+            left: false,
+            right: false,
+            up: true,
+            down: true
+          }
         }
       }
-    }
-    //key arrow right pressed
-    if (39 in glob.keys) {
-      if (player.movements.right) {
-        player.x += player.speed * modifier;
-        player.direction = DIRECTIONS.RIGHT;
-        player.movements = {
-          left: false,
-          right: false,
-          up: true,
-          down: true
-        }
-      }
-    }
-    //key arrow down pressed
-    if (40 in glob.keys) {
-      if (player.movements.down) {
-        player.y += player.speed * modifier;
-        player.direction = DIRECTIONS.DOWN;
-        player.movements = {
-          left: true,
-          right: true,
-          up: false,
-          down: false
+      //key arrow down pressed
+      // if (40 in glob.keys) {
+      if (40 === glob.key) {
+        if (player.movements.down) {
+          player.y += player.speed * modifier;
+          player.direction = DIRECTIONS.DOWN;
+          player.movements = {
+            left: true,
+            right: true,
+            up: false,
+            down: false
+          }
         }
       }
     }
@@ -229,9 +235,7 @@ var __width = 600,
   }
   glob.updateGame = updateGame;
 
-  glob.player = player;
-  glob.npc = npc;
-  glob.score = score;
+  glob.player = player; glob.npc = npc; glob.score = score;
 
   function mainLoop() {
     var now = Date.now(),
@@ -245,11 +249,8 @@ var __width = 600,
     //Request the update ASAP
     requestAnimationFrame(mainLoop);
   }
-  glob.ctx = ctx;
-  glob.mainLoop = mainLoop;
-  glob.then = Date.now();
+  glob.ctx = ctx; glob.mainLoop = mainLoop; glob.then = Date.now();
 
   //FIXME: Change this for a button call or something
-  glob.resetScenario();
-  glob.mainLoop();
+  glob.resetScenario(); glob.mainLoop();
 })(glob.canvas);
